569ef80b42d15faa77fd6016d8b746f692e275a3
mounikapriyaguddanti <mounikapriyaguddanti@gmail.com>
2024-04-12 09:26:02 +0530

    intial commit

---
+++ tribute-site/src/components/Quotes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const Quotes= () => {
+  return (
+    <div className="quotes">
+            <h2>Inspiring Quotes</h2>
+            <div className="quote">
+              <blockquote>
+                <p>
+                  "Dream is not that which you see while sleeping, it is something
+                  that does not let you sleep."
+                </p>
+                <cite>- Dr. A.P.J. Abdul Kalam</cite>
+              </blockquote>
+            </div>
+            <div className="quote-container">
+              <blockquote>
+                <p>
+                  "You have to dream before your dreams can come true."
+                </p>
+                <cite>- Dr. A.P.J. Abdul Kalam</cite>
+              </blockquote>
+            </div>
+            <div className="quote-container">
+              <blockquote>
+                <p>
+                  "If you fail, never give up because F.A.I.L. means 'First
+                  Attempt In Learning'."
+                </p>
+                <cite>- Dr. A.P.J. Abdul Kalam</cite>
+              </blockquote>
+            </div>
+            <div className="quote-container">
+              <blockquote>
+                <p>
+                  "The best way to find yourself is to lose yourself in the
+                  service of others."
+                </p>
+                <cite>- Dr. A.P.J. Abdul Kalam</cite>
+              </blockquote>
+            </div>
+          </div>
+ 
+    );
+};
+
+export default Quotes;
